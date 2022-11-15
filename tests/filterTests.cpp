d67811deeb93f84f555ec4671e9fa3e8c12726f2
maneesh.sutar <maneesh.sutar@thoughtworks.com>
2022-11-15 18:34:44 +0530

    Maneesh / Maniaddhanki : Replaced all tests with gtest

--- tests/filterTests.cpp
+++ tests/filterTests.cpp
@@ -6,26 +6,6 @@
 
 using std::vector;
 
-void shouldReturnAverageFilteredValuesWhenInputIsProvided()
-{
-    int inputSize, filterSize;
-
-    inputSize = 10;
-    filterSize = 5;
-
-    float expectedOutput[] = {0, 0, 3, 4, 5, 6, 7, 8, 0, 0};
-
-    vector<float> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-    vector<float> output;
-    output = filter(input, inputSize, filterSize, averageFilter);
-
-    for (int i = 0; i < sizeof(expectedOutput) / sizeof(expectedOutput[0]); i++)
-    {
-        assert(output[i] == expectedOutput[i]);
-    }
-}
-
 TEST(filterTests, shouldReturnAverageFilteredValuesWhenInputIsProvided)
 {
     int inputSize, filterSize;
@@ -33,83 +13,40 @@ TEST(filterTests, shouldReturnAverageFilteredValuesWhenInputIsProvided)
     inputSize = 10;
     filterSize = 5;
 
-    float expectedOutput[] = {0, 0, 3, 4, 5, 6, 7, 8, 0, 0};
+    vector<float> expectedOutput{0, 0, 3, 4, 5, 6, 7, 8, 0, 0};
 
     vector<float> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    vector<float> output;
-    output = filter(input, inputSize, filterSize, averageFilter);
+    vector<float> output = filter(input, inputSize, filterSize, averageFilter);
 
-    for (int i = 0; i < sizeof(expectedOutput) / sizeof(expectedOutput[0]); i++)
+    EXPECT_EQ(output.size(), expectedOutput.size());
+    for (int i = 0; i < output.size(); i++)
     {
-        EXPECT_EQ(output[i], expectedOutput[i]);
+        EXPECT_EQ(output[i] , expectedOutput[i]);
     }
 }
 
-void shouldReturnMedianFilteredValuesWhenInputIsProvided()
+TEST(filterTests, shouldReturnMedianFilteredValuesWhenInputIsProvided)
 {
     int inputSize, filterSize;
 
     inputSize = 10;
     filterSize = 5;
 
-    float expectedOutput[] = {0, 0, 3, 4, 5, 6, 7, 8, 0, 0};
+    vector<float> expectedOutput{0, 0, 3, 4, 5, 6, 7, 8, 0, 0};
 
     vector<float> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     vector<float> output = filter(input, inputSize, filterSize, medianFilter);
 
-    for (int i = 0; i < sizeof(expectedOutput) / sizeof(expectedOutput[0]); i++)
+    EXPECT_EQ(output.size(), expectedOutput.size());
+    for (int i = 0; i < output.size(); i++)
     {
-        assert(output[i] == expectedOutput[i]);
+        EXPECT_EQ(output[i] , expectedOutput[i]);
     }
 }
 
-void shouldThrowErrorWhenFilterSizeIsGreaterThanInputSize()
-{
-    int inputSize, filterSize;
-
-    inputSize = 9;
-    filterSize = 20;
-
-    vector<float> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-    bool invalidArgumentExceptionThrown = false;
-    try
-    {
-        filter(input, inputSize, filterSize, averageFilter);
-    }
-    catch (std::invalid_argument &e)
-    {
-        invalidArgumentExceptionThrown = true;
-    }
-
-    assert(invalidArgumentExceptionThrown);
-}
-
-void shouldThrowErrorWhenFilterSizeIsLessThanOrEqualToZero()
-{
-    int inputSize, filterSize;
-
-    inputSize = 9;
-    filterSize = 0;
-
-    vector<float> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-    bool invalidArgumentExceptionThrown = false;
-    try
-    {
-        filter(input, inputSize, filterSize, averageFilter);
-    }
-    catch (std::invalid_argument &e)
-    {
-        invalidArgumentExceptionThrown = true;
-    }
-
-    assert(invalidArgumentExceptionThrown);
-}
-
-void shouldReturnPaddedAverageFilteredValuesWhenInputIsProvided()
+TEST(filterTests, shouldReturnPaddedAverageFilteredValuesWhenInputIsProvided)
 {
     int inputSize, filterSize;
 
@@ -117,18 +54,19 @@ void shouldReturnPaddedAverageFilteredValuesWhenInputIsProvided()
     filterSize = 5;
 
     vector<float> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    float expectedOutput[] = {1.2, 2, 3, 4, 5, 6, 7, 8, 6.8, 5.4};
+    vector<float> expectedOutput{1.2, 2, 3, 4, 5, 6, 7, 8, 6.8, 5.4};
 
     vector<float> output;
     output = filter(input, inputSize, filterSize, paddedAverageFilter);
 
-    for (int i = 0; i < sizeof(expectedOutput) / sizeof(expectedOutput[0]); i++)
+    EXPECT_EQ(output.size(), expectedOutput.size());
+    for (int i = 0; i < output.size(); i++)
     {
-        assert(output[i] == expectedOutput[i]);
+        EXPECT_EQ(output[i] , expectedOutput[i]);
     }
 }
 
-void shouldReturnPaddedMedianFilteredValuesWhenInputIsProvided()
+TEST(filterTests, shouldReturnPaddedMedianFilteredValuesWhenInputIsProvided)
 {
     int inputSize, filterSize;
 
@@ -136,18 +74,19 @@ void shouldReturnPaddedMedianFilteredValuesWhenInputIsProvided()
     filterSize = 5;
 
     vector<float> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    float expectedOutput[] = {1, 2, 3, 4, 5, 6, 7, 8, 8, 8};
+    vector<float> expectedOutput{1, 2, 3, 4, 5, 6, 7, 8, 8, 8};
 
     vector<float> output;
     output = filter(input, inputSize, filterSize, paddedMedianFilter);
 
-    for (int i = 0; i < sizeof(expectedOutput) / sizeof(expectedOutput[0]); i++)
+    EXPECT_EQ(output.size(), expectedOutput.size());
+    for (int i = 0; i < output.size(); i++)
     {
-        assert(output[i] == expectedOutput[i]);
+        EXPECT_EQ(output[i] , expectedOutput[i]);
     }
 }
 
-void shouldReturnMedianFilteredValuesWithStdNthElementFunctionWhenInputIsProvided()
+TEST(filterTests, shouldReturnMedianFilteredValuesWithStdNthElementFunctionWhenInputIsProvided)
 {
     int inputSize, filterSize;
 
@@ -155,14 +94,15 @@ void shouldReturnMedianFilteredValuesWithStdNthElementFunctionWhenInputIsProvide
     filterSize = 5;
 
     vector<float> input{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    float expectedOutput[] = {0, 0, 3, 4, 5, 6, 7, 8, 0, 0};
+    vector<float> expectedOutput{0, 0, 3, 4, 5, 6, 7, 8, 0, 0};
 
     vector<float> output;
     output = filter(input, inputSize, filterSize, medianFilterWithStdNthElementFunction);
 
-    for (int i = 0; i < sizeof(expectedOutput) / sizeof(expectedOutput[0]); i++)
+    EXPECT_EQ(output.size(), expectedOutput.size());
+    for (int i = 0; i < output.size(); i++)
     {
-        assert(output[i] == expectedOutput[i]);
+        EXPECT_EQ(output[i] , expectedOutput[i]);
     }
 }
 
@@ -193,7 +133,5 @@ TEST(filterTests, shouldReturnAverageTwoDFilteredValuesWhenInputIsProvided)
         {
             EXPECT_EQ(output[i][j], expectedOutput[i][j]);
         }
-        
     }
-
 }
--- tests/test-run.cpp
+++ tests/test-run.cpp
@@ -1,27 +1,9 @@
-#include "filterTests.hpp"
-
 #include <chrono>
 #include <gtest/gtest.h>
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-    std::cout << "performing tests ..." << std::endl;
-
-    shouldReturnAverageFilteredValuesWhenInputIsProvided();
-
-    shouldReturnMedianFilteredValuesWhenInputIsProvided();
-
-    // shouldThrowErrorWhenFilterSizeIsGreaterThanInputSize();
-    // shouldThrowErrorWhenFilterSizeIsLessThanOrEqualToZero();
-
-    shouldReturnPaddedAverageFilteredValuesWhenInputIsProvided();
-    shouldReturnPaddedMedianFilteredValuesWhenInputIsProvided();
-
-    shouldReturnMedianFilteredValuesWithStdNthElementFunctionWhenInputIsProvided();
-    
-    std::cout << std::endl << "all tests passed ..." << std::endl;
-
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 
